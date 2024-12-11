77002269a0dc67bde07aedc48a153e3f4d7da34d
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-12-11 15:02:32 +0100

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Comments } from './components/Comments/Comments';
 
 function App() {
   return (
-    <div className="flex items-center justify-center w-full h-full p-4 bg-zinc-50">
+    <div className="flex items-center justify-center w-full h-full min-h-dvh p-4 bg-zinc-50">
       <Comments />
     </div>
   );
